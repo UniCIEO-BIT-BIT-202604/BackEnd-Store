0f10989a06a788ed11ec161d9e8dae1df69e21ce
Juan Carlos Jiménez Gutiérrez <jangojes.dev@gmail.com>
2026-05-19 19:58:32 -0500

    [+] index.js: Define GET /health

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ const express = require( 'express' );
 
 const app = express();
 
+// Endpoint
+app.get( '/health', ( req, res ) => {
+    res.json({
+        msg: 'Sitio funcionando! :)'
+    });
+} );
+
 // Lanzo el servidor web
 app.listen( 3000, () => {
     console.log( `Server running on http://localhost:3000` );
