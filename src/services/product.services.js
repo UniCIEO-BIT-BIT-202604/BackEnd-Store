368c96f55b3cf09f57378bfcdd3fee41f80e920c
Juan Carlos Jiménez Gutiérrez <jangojes.dev@gmail.com>
2026-07-24 17:48:54 -0500

    [$] Product.model.js => product.model.js (Replace files) / product.service.js => product.services.js (Rename File)

--- src/services/product.services.js
+++
@@ -1,9 +0,0 @@
-import ProductModel from "../models/product.model.js";
-
-
-
-const insertProducts =   async (newProduct)=>{
-    return await  ProductModel.create(newProduct);
-};
-
-export  {insertProducts}
